6b9bb65c1654ad63efc582493fca7bedf4c32dca
Divyansh Agarwal <DivyanshAgarwal24@mittymonarch.com>
2022-01-11 16:08:01 -0800

    shooter structure

--- src/main/java/com/github/mittyrobotics/shooter/ShooterSubsystem.java
+++ src/main/java/com/github/mittyrobotics/shooter/ShooterSubsystem.java
@@ -1,2 +1,30 @@
-package com.github.mittyrobotics.shooter.commands;public class ShooterSubsystem {
+package com.github.mittyrobotics.shooter;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    private static  ShooterSubsystem instance;
+
+    private ShooterSubsystem () {
+        setName("Shooter");
+    }
+
+    public static ShooterSubsystem getInstance() {
+        if (instance == null) {
+            instance = new ShooterSubsystem();
+        }
+        return instance;
+    }
+
+    /**
+     * HARDWARE HERE
+     */
+
+    public void initHardware() {
+
+    }
+
+    public void updateDashboard() {
+
+    }
 }
